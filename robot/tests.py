a4a1a35121aee1b4bfbd32dc7da9ea4c185205d8
Admin <admin@example.com>
2017-07-12 08:17:28 -0300

    Text output tests added

--- robot/tests.py
+++ robot/tests.py
@@ -5,30 +5,38 @@ import requests
 
 class RobotTestCase(unittest.TestCase):
 
-    def testRobotServer200(self):
+    def testRobotServerRest200(self):
         response = requests.post(url='http://localhost:8080/rest/mars/MML')
         self.assertEqual(requests.codes.ok, response.status_code)
 
+    def testRobotServerText200(self):
+        response = requests.post(url='http://localhost:8080/text/mars/MML')
+        self.assertEqual(requests.codes.ok, response.status_code)
+
     def testRobotWorkingWellResponseJson(self):
-        content = requests.get(url='http://localhost:8080/rest/mars/MMRMMRMM')
+        content = requests.post(url='http://localhost:8080/rest/mars/MMRMMRMM')
         self.assertEqual(2, json.loads(content.text)['x'])
         self.assertEqual(0, json.loads(content.text)['y'])
         self.assertEqual('S', json.loads(content.text)['d'])
 
+    def testRobotWorkingWellResponseText(self):
+        content = requests.post(url='http://localhost:8080/text/mars/MMRMMRMM')
+        self.assertEqual('(2, 0, S)', content.text)
+
     def testRobotCompleteCircleToLeft(self):
-        content = requests.get(url='http://localhost:8080/rest/mars/LLLL')
+        content = requests.post(url='http://localhost:8080/rest/mars/LLLL')
         self.assertEqual(0, json.loads(content.text)['x'])
         self.assertEqual(0, json.loads(content.text)['y'])
         self.assertEqual('N', json.loads(content.text)['d'])
 
     def testRobotCompleteCircleToRight(self):
-        content = requests.get(url='http://localhost:8080/rest/mars/RRRR')
+        content = requests.post(url='http://localhost:8080/rest/mars/RRRR')
         self.assertEqual(0, json.loads(content.text)['x'])
         self.assertEqual(0, json.loads(content.text)['y'])
         self.assertEqual('N', json.loads(content.text)['d'])
 
     def testRobotWalkingOnTheBoardAndBackToOrigin(self):
-        content = requests.get(url='http://localhost:8080/rest/mars/MMMMMRMMMMMRMMMMMRMMMMMR')
+        content = requests.post(url='http://localhost:8080/rest/mars/MMMMMRMMMMMRMMMMMRMMMMMR')
         self.assertEqual(0, json.loads(content.text)['x'])
         self.assertEqual(0, json.loads(content.text)['y'])
         self.assertEqual('N', json.loads(content.text)['d'])
