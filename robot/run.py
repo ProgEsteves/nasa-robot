df99e86706d29571c3f439c336031ffcf8fabaca
Admin <admin@example.com>
2017-07-12 08:16:43 -0300

    Text output added

--- robot/run.py
+++ robot/run.py
@@ -8,5 +8,10 @@ def appRestMarsJson(cmd):
 	from robot import Robot
 	return Robot(cmd).getLocation()
 
+@app.route('/text/mars/<cmd>', methods=['GET','POST'])
+def appRestMarsText(cmd):
+	from robot import Robot
+	return Robot(cmd).getLocationText()
+
 if __name__ == '__main__':
 	app.run(host='0.0.0.0',port=8080)
